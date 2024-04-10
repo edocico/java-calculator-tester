84bf5fe430fd55fe86d2e2cb63a18247906f97ba
edocico <138378767+edocico@users.noreply.github.com>
2024-04-10 16:29:40 +0200

    add NaN exception and tests

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -10,21 +10,33 @@ public class Calculator {
     // metodi
 
     public float add(float num1, float num2) {
+       if (Float.isNaN(num1) || Float.isNaN(num2)) {
+           throw new IllegalArgumentException("un argomento è NaN");
+       }
         return num1 + num2;
     }
 
     public float subtract(float num1, float num2) {
+        if (Float.isNaN(num1) || Float.isNaN(num2)) {
+            throw new IllegalArgumentException("un argomento è NaN");
+        }
         return num1 - num2;
     }
 
     public float divide(float num1, float num2) throws IllegalArgumentException {
-        if (num2 == 0) {
+        if (Float.isNaN(num1) || Float.isNaN(num2)) {
+            throw new IllegalArgumentException("un argomento è NaN");
+        }
+        if (num2 == 0.0f) {
             throw new IllegalArgumentException("un qualsiasi numero non può essere divisibile per 0");
         }
         return num1/num2;
     }
 
     public float multiply(float num1, float num2) {
+        if (Float.isNaN(num1) || Float.isNaN(num2)) {
+            throw new IllegalArgumentException("un argomento è NaN");
+        }
         return num1*num2;
     }
 
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -21,6 +21,7 @@ class CalculatorTest {
         System.out.println("test add function");
         assertEquals(6.2f, calculator.add(3.1f, 3.1f));
         assertEquals(-2.2f, calculator.add(-4.4f, 2.2f));
+        assertThrows(IllegalArgumentException.class, () -> calculator.add(Float.NaN, 1.0f ));
     }
 
     @Test
@@ -28,6 +29,7 @@ class CalculatorTest {
         System.out.println("test subtract function");
         assertEquals(2.0f, calculator.subtract(6.0f, 4.0f));
         assertEquals(4.4f, calculator.subtract(2.2f, -2.2f));
+        assertThrows(IllegalArgumentException.class, ()-> calculator.subtract(Float.NaN, 2.0f));
     }
 
     @Test
@@ -36,11 +38,14 @@ class CalculatorTest {
 
         assertThrows(IllegalArgumentException.class, () -> calculator.divide(4, 0), "è impossibile dividere un qualsiasi " +
                 "numero per 0");
+        assertThrows(IllegalArgumentException.class,()-> calculator.divide(Float.NaN, 2f));
         assertEquals(12f, calculator.divide(24f, 2f));
     }
 
     @Test
     public void multiply() {
         assertEquals(49f, calculator.multiply(7f,7f));
+        assertEquals(4.84f, calculator.multiply(2.2f, 2.2f));
+        assertThrows(IllegalArgumentException.class, ()-> calculator.multiply(Float.NaN, 2f));
     }
 }
